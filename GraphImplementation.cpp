15df795fa320c1eaa6f93a3f6e7fde9242de0661
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-06-18 21:48:45 +0530

    Implemented graph using adjacency list representation.

---
+++ GraphImplementation.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int>*l;
+
+    public:
+    Graph(int V){
+        this->V=V;
+        l=new list<int>[V];
+    }
+    void addEdge(int x,int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+    void print(){
+        for(int i=0; i<V; i++){
+            cout<<"vertex "<<i<<"->";
+            for(int nbr:l[i]){
+                cout<<nbr<<",";
+            }
+            cout<<endl;
+        }
+    }
+
+};
+
+int main(){
+
+    Graph gr(4);
+    
+    gr.addEdge(0,1);
+    gr.addEdge(0,2);
+    gr.addEdge(2,3);
+    gr.addEdge(1,2);
+
+    gr.print();
+
+    return 0;
+}
