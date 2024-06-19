96e92e228f9f5b978a11b634da0ee28848938348
RajeevRanjan5642 <rajeevranjan31012003@gmail.com>
2024-06-19 11:08:57 +0530

    implemented graph using adjacency list as array of lists

--- GraphImplementation.cpp
+++ GraphImplementation.cpp
@@ -1,40 +1,49 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
+
 using namespace std;
 
-class Graph{
+class Graph
+{
     int V;
-    list<int>*l;
+    list<int> *l;
 
-    public:
-    Graph(int V){
-        this->V=V;
-        l=new list<int>[V];
+public:
+    Graph(int V)
+    {
+        this->V = V;
+        l = new list<int>[V];
     }
-    void addEdge(int x,int y){
+
+    void addEdge(int x, int y)
+    {
         l[x].push_back(y);
         l[y].push_back(x);
     }
-    void print(){
-        for(int i=0; i<V; i++){
-            cout<<"vertex "<<i<<"->";
-            for(int nbr:l[i]){
-                cout<<nbr<<",";
+
+    void print()
+    {
+        for (int i = 0; i < V; i++)
+        {
+            cout << "vertex " << i << "->";
+            for (int nbr : l[i])
+            {
+                cout << nbr << ",";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-
 };
 
-int main(){
+int main()
+{
 
     Graph gr(4);
-    
-    gr.addEdge(0,1);
-    gr.addEdge(0,2);
-    gr.addEdge(2,3);
-    gr.addEdge(1,2);
+
+    gr.addEdge(0, 1);
+    gr.addEdge(0, 2);
+    gr.addEdge(2, 3);
+    gr.addEdge(1, 2);
 
     gr.print();
 
